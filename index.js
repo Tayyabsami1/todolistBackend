93abb5ba040719f1dc4c94400e960a00591d841b
M. Tayyab <tayyabnassem246@gmail.com>
2024-08-12 19:33:19 +0500

    Authentication Logic (1.0)

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express=require('express');
 const app=express();
 const cors=require("cors");
+const authRoutes = require('./routes/authRoutes')
+const {Connect_to_Mongo_DB}=require('./connect');
+const cookieParser = require('cookie-parser')
+const {requireAuth} = require('./middleware/authMiddleware')
 require('dotenv').config();
 
-const Atlas=process.env.DB
 //DB connection
-const {Connect_to_Mongo_DB}=require('./connect');
-Connect_to_Mongo_DB(Atlas);
+//Usman's Code 
+// const Atlas=process.env.DB
+// Connect_to_Mongo_DB(Atlas);
+
+//Local Database Connection Code
+//Connecting to Database
+const connectionString = 'mongodb://localhost:27017/';
+const databaseName = 'To_Do_List';
+const url = connectionString + databaseName;
+Connect_to_Mongo_DB(url);
+
+
 //Router Connections
-//const ExampleRouter=require('./routes/example');
+
 app.use(express.json());
 app.use(cors());
-//app.use('/example',ExampleRouter);
+app.use(authRoutes);
+app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    res.json("Welcome to To Do List)");
+
+
+app.get("/", requireAuth ,(req,res)=>{
+    res.json("To Do list running at localhost:3000");
 })
+
 //Listening to APP through PORT 3000.
 app.listen(process.env.PORT || 3000,()=>{
-console.log("Server Started at PORT:Atlas ");
+console.log("Server Started at local host");
 });
