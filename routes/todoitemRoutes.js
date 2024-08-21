ce0cd5b57704364f8606041fefc446b5e41fd1bd
M. Tayyab <tayyabnassem246@gmail.com>
2024-08-21 22:52:55 +0500

    Basic to-do item crud operations

---
+++ routes/todoitemRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const todoitemController = require('../controllers/todoitemController')
+const router = express.Router();
+
+
+router.post('/todoitem/create',todoitemController.createToDoItem)
+router.delete('/todos/:listTitle/:itemId', todoitemController.deleteToDoItem);
+router.get('/todoitem/total_items',todoitemController.getTotalItems)
+router.get('/todoitem/pending_items',todoitemController.getPendingItems)
+router.get('/todo/completed_items',todoitemController.getCompletedItems)
+
+module.exports = router
